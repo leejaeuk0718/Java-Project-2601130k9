782a0cee9a83eba8220a757193419da7e04f12f3
lsy3709 <lsy3709@naver.com>
2026-01-19 11:25:53 +0900

    3교시, 상속, 생성자 호출 흐름 실습1

---
+++ src/_5_test_260119/ex/_1_A_Ex1.java
@@ -0,0 +1,8 @@
+package _5_test_260119.ex;
+
+public class _1_A_Ex1 {
+    //부모클래스
+    public _1_A_Ex1() {
+        System.out.println("_1_A_Ex1 (부모)클래스 생성자");
+    }
+}
---
+++ src/_5_test_260119/ex/_1_Main_Ex1.java
@@ -0,0 +1,14 @@
+package _5_test_260119.ex;
+
+public class _1_Main_Ex1 {
+    public static void main(String[] args) {
+//        1)
+//        부모 → 자식 생성자 호출 순서를 출력
+//        부모 클래스 : A
+//        자식 클래스 : B
+//        각각의 생성자를 만들기,
+//        실행 클래스 : Main ,
+        _1_B_Ex1 b = new _1_B_Ex1();
+
+    }
+}
